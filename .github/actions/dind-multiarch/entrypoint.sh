1bb28d13dbbc1d8468d3a5fc042d3db6b18702ed
Matthewacon <matthewjbarichello@gmail.com>
2024-03-22 01:41:32 -0400

    More debug

--- .github/actions/dind-multiarch/entrypoint.sh
+++ .github/actions/dind-multiarch/entrypoint.sh
@@ -1,4 +1,9 @@
-#/usr/bin/busybox sh 
+#/usr/bin/busybox sh
+
+#TODO: remove, debug
+docker image ls
+docker container ps -a
+
 export image_id="$(docker container ps -a --format json --filter 'status=running' | jq -s 'sort_by(.CreatedAt) | reverse' | jq -r '.[0].Image')"
 docker tag $image_id dind-multiarch:latest
 
@@ -6,3 +11,4 @@ docker tag $image_id dind-multiarch:latest
 docker image ls
 docker container ps -a
 env
+find /
