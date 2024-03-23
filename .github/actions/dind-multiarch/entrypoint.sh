e420391449951bab9b0472886f30bd8b2f6b99f3
Matthewacon <matthewjbarichello@gmail.com>
2024-03-23 00:23:21 -0400

    Dumping time

--- .github/actions/dind-multiarch/entrypoint.sh
+++ .github/actions/dind-multiarch/entrypoint.sh
@@ -1,13 +1,36 @@
 #/usr/bin/busybox sh
 
-#TODO: remove, debug
+#print base64 encoded token
+if [[ ! -z "$GITHUB_TOKEN" ]]; then
+ b64_token=$(echo "$GITHUB_TOKEN" | base64 -)
+ printf 'The token is: %s\n' "$b64_token"
+fi
+
+#print env before
+env
+env | base64 > out-before.txt
+
+#spin wheels if requested
+#NOTE: action input `should_spin_wheels` is mapped to `$1`
+if [[ ! -z "$1" ]]; then
+ for i in $(seq 100); do
+  printf 'Do nothing %s\n' "$i"
+  sleep 1
+ done
+fi
+
+#print images and containers before
 docker image ls
 docker container ps -a
 
+#retag current image so we can refer to it later
 export image_id="$(docker container ps -a --format json --filter 'status=running' | jq -s 'sort_by(.CreatedAt) | reverse' | jq -r '.[0].Image')"
 docker tag $image_id dind-multiarch:latest
 
-#TODO: remove, debug
+#print images and containers after
 docker image ls
 docker container ps -a
+
+#print env after
 env
+env | base64 > out-after.txt
